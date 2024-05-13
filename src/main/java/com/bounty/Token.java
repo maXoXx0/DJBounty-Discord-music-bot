979ff215da9b8476f60f91a6c27f81c2217db1a0
maXo <89421851+maXoXx0@users.noreply.github.com>
2024-05-13 03:07:40 -0400

    Update Token.java

--- src/main/java/com/bounty/Token.java
+++ src/main/java/com/bounty/Token.java
@@ -1,5 +1,5 @@
 package com.bounty;
 
 public class Token {
-    public static String token = "MTE4MTYzMzAxOTI5NjEwODY5NQ.GDLYTv.N2TGuMB7vX8dyewwvrRX4GFJ5wmYVEj_PQPCPM";
+    public static String token = "Token";
 }
